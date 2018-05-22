8120406945f4131f6530aac81b49124e9d8d88f3
PsyBurn <psykill@gmail.com>
2018-05-22 23:51:09 +0200

    make it pretty, move to another component

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
+import { ICharacter } from '../../mockDataInterface';
 import { list } from './../../mock';
 import './list.css';
 
+interface IListItem {
+  data: ICharacter;
+}
+class ListItem extends React.Component<IListItem> {
+  public render() {
+    const { data } = this.props;
+    return <div className='List-Item'>
+      <img src={data.image} className='List-Item-Image' />
+      <div className='List-Item-Text'>{data.name}</div>
+    </div>
+  }
+}
 export default class List extends React.Component<{}> {
     public render() {
       const results = list.results;
-      if (!list) {
-        return null;
+      if (!results.length) {
+        return <div> Nothing here - move along</div>;
       }
       return <div>{
-        results.map((singleResult) => (<div className='List-Item'>{singleResult.name}</div>))
+        results.map((singleResult, index) => <ListItem key={index} data={singleResult} />)
       }</div>;
     }
 }
